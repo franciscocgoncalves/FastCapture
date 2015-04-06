0ebe684a547d3f6cccae38c1f18a7a176417f3ad
Francisco Gonçalves <franciscocgoncalves@gmail.com>
2015-04-06 17:47:48 +0100

    Initial Commit

---
+++ FastCapture/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  FastCapture
+//
+//  Created by Francisco Gonçalves on 06/04/15.
+//  Copyright (c) 2015 Francisco Gonçalves. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
